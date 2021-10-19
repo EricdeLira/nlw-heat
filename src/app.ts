5668e68e215748d27f4f6fa7f636674a37fe7ad8
Eric de Lira Candido <ericdelira2001@gmail.com>
2021-10-18 21:26:31 -0300

    Rota de login com github

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
+import "dotenv/config"; // Acessar variáveis de ambiente
+
 import express from "express";
 
 const app = express();
+
+app.get("/github", (request, response) => {
+    response.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+    );
+});
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query;
+
+    return response.json(code);
+});
 
 app.listen(4000, () => { console.log('Server running on PORT 4000')});
 
